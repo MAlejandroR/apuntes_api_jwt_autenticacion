e9a6ce535bd922fc413e51ca3c70e81cccf13d04
MAlejandro <manuelromeromiguel@gmail.com>
2023-12-11 17:07:49 +0100

    actualizado para subir a nett

--- despliegue.sh
+++ despliegue.sh
@@ -1,8 +1,5 @@
-git add *
-git commit -m "actualizando contenido"
-git push origin main
-
 sudo rm ./public -r
 hugo -d public
-rsync -avz -e "ssh -p 22022" ./public/ inaem@infenlaces.com:web/certificado.infenlaces.com/public_html/contenido/
+rsync -avz -e "ssh" ./public/ profesor@proyectosdwa.es:httpdocs/manuel/api
+
 
